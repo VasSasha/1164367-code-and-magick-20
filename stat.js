a25d8edf4ad72873f29a3e3b39e6e043523f67bf
VasSasha <65274003+VasSasha@users.noreply.github.com>
2020-06-05 19:05:39 +0700

    update stat.js

---
+++ stat.js
@@ -0,0 +1,13 @@
+ 
+var canvas = document.gatElementById('canvas');
+var ctx= canvas.getContext('2d');
+var CLOUD_WIDTH = 420;
+var CLOUD_HEIGHT =270;
+var renderCloud = function(ctx,x,y,color){
+	ctx.fillStyle = color;
+	ctx.fillRect(x,y,CLOUD_WIDTH, CLOUD_HEIGHT);
+}
+window.renderStatistics = function(ctx){
+	renderCloud(ctx,110,20,'rgba(0, 0, 0, 0.7)');
+	renderCloud(ctx, 100, 10, '#fff');
+}
