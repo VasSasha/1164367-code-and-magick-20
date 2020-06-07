e4c26bcce71bbc8f297dba53ab19f44fa1613fe5
VasSasha <65274003+VasSasha@users.noreply.github.com>
2020-06-07 23:29:10 +0700

    Update stat.js

--- js/stat.js
+++ js/stat.js
@@ -5,28 +5,41 @@ var CLOUD_Y =10;
 var GAP = 10;
 var FONT_GAP = 16;
 var TEXT_WIDTH = 50;
-var BAR_WIDTH = 50;
+var BAR_WIDTH = 40;
+var BAR_GAP = 50;
 var TEXT_HEIGHT = 20;
 var barHeight = CLOUD_HEIGHT- TEXT_HEIGHT - GAP*10;
-var maxBarHeight = 200;
+var maxBarHeight = 150;
 var renderCloud = function(ctx,x,y,color){
 	ctx.fillStyle = color;
 	ctx.fillRect(x,y,CLOUD_WIDTH, CLOUD_HEIGHT);
 }
-window.renderStatistics = function(ctx,players){
+var getMaxElement = function(arr){
+		var maxElement = arr[0];
+		for(var i=0; i< arr.length; i++){
+		if(arr[i]> maxElement){
+			maxElement = arr[i];
+		}
+	} return maxElement;
+}
+window.renderStatistics = function(ctx,players,times){
 	renderCloud(ctx,CLOUD_X + GAP, CLOUD_Y + GAP,'rgba(0, 0, 0, 0.7)');
 	renderCloud(ctx, CLOUD_X, CLOUD_Y, '#fff');
 	ctx.fillStyle = '#000';
 	ctx.fillText('Ура вы победили!',CLOUD_X + GAP,CLOUD_Y*4);
-	ctx.fillText('Список результатов:',CLOUD_X + GAP,CLOUD_Y*5);
+	ctx.fillText('Список результатов:',CLOUD_X + GAP,CLOUD_Y*5.3);
+	var maxTime = getMaxElement(times);
 	for(var i = 0; i< players.length; i++){
-		ctx.fillStyle = '#000';
-		ctx.fillRect(CLOUD_X + GAP +(GAP+ TEXT_WIDTH)* i, CLOUD_Y +GAP*5 +TEXT_HEIGHT*2, BAR_WIDTH,CLOUD_HEIGHT*0.53);
-		ctx.fillText(players[i],CLOUD_X + GAP +(GAP+ TEXT_WIDTH)* i, CLOUD_HEIGHT-CLOUD_HEIGHT* 0.05);
-	}
-	if(players[i] === 'Вы'){
+		if(players[i] === 'Вы'){
 		ctx.fillStyle = 'rgba(255, 0, 0, 1)';
 	}else{
+		ctx.fillStyle = 'rgb(64,83,145)';
+	}
+		ctx.fillRect(CLOUD_X + GAP +(BAR_GAP+ TEXT_WIDTH)* i, CLOUD_Y +TEXT_HEIGHT*2 + 3*GAP, BAR_WIDTH,(barHeight*times[i])/maxTime);
 		ctx.fillStyle = '#000';
+		ctx.fillText(players[i],CLOUD_X + GAP +(BAR_GAP+ TEXT_WIDTH)* i, CLOUD_HEIGHT* 0.95);
+	if(barHeight<maxBarHeight){
+		ctx.fillRect(CLOUD_X + GAP +(BAR_GAP+ TEXT_WIDTH)* i, CLOUD_HEIGHT* 0.75, BAR_WIDTH,(barHeight*times[i])/maxTime)
+	}
 	}
 }
