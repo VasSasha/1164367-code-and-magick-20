7135b8762a3082297be53f4ae46e1e49b6e92cac
VasSasha <65274003+VasSasha@users.noreply.github.com>
2020-06-08 01:52:37 +0700

    Update stat.js

--- js/stat.js
+++ js/stat.js
@@ -7,8 +7,6 @@ var FONT_GAP = 16;
 var TEXT_WIDTH = 50;
 var BAR_WIDTH = 40;
 var BAR_GAP = 50;
-var TEXT_HEIGHT = 20;
-var barHeight = CLOUD_HEIGHT- TEXT_HEIGHT - GAP*10;
 var maxBarHeight = 150;
 var renderCloud = function(ctx,x,y,color){
 	ctx.fillStyle = color;
@@ -26,7 +24,7 @@ window.renderStatistics = function(ctx,players,times){
 	renderCloud(ctx,CLOUD_X + 10, CLOUD_Y + 10,'rgba(0, 0, 0, 0.7)');
 	renderCloud(ctx, CLOUD_X, CLOUD_Y, '#fff');
 	ctx.textBaseline='hanging';
-	ctx.font='16px Mono';
+	ctx.font='16px "PT Mono"';
 	ctx.fillStyle = '#000';
 	ctx.fillText('Ура вы победили!',CLOUD_X + 35,CLOUD_Y+ 12);
 	ctx.fillText('Список результатов:',CLOUD_X + 35,CLOUD_Y + 12 + FONT_GAP);
@@ -38,8 +36,8 @@ window.renderStatistics = function(ctx,players,times){
 		var X = Math.random()*100;
 		ctx.fillStyle = 'hsl(240,'+X+'%, 50%)';
 	}
-		ctx.fillRect(CLOUD_X + GAP +(BAR_GAP+ TEXT_WIDTH)* i, CLOUD_Y +TEXT_HEIGHT*2 + 3*GAP, BAR_WIDTH,(barHeight*times[i])/maxTime);
+		ctx.fillRect(CLOUD_X + BAR_GAP +(BAR_GAP+ TEXT_WIDTH)* i,CLOUD_Y +FONT_GAP*2 + 30+ maxBarHeight- (maxBarHeight*times[i])/maxTime, BAR_WIDTH,(maxBarHeight*times[i])/maxTime);
 		ctx.fillStyle = '#000';
-		ctx.fillText(players[i],CLOUD_X + GAP +(BAR_GAP+ TEXT_WIDTH)* i, CLOUD_HEIGHT* 0.95);
+		ctx.fillText(players[i],CLOUD_X + BAR_GAP +(BAR_GAP+ TEXT_WIDTH)* i, CLOUD_HEIGHT-10- FONT_GAP);
 	}
 }
