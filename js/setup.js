c87ec61da5ea549cb8dfcb30cec2303ba135c38e
VasSasha <65274003+VasSasha@users.noreply.github.com>
2020-06-12 00:13:14 +0700

    Update setup.js

--- js/setup.js
+++ js/setup.js
@@ -2,43 +2,10 @@
 var similiarWizardTemplate = document.querySelector('#similar-wizard-template').content;
 var setup = document.querySelector('.setup');
 setup.classList.remove('hidden');
-var playersCharacteristics = [
-    name {
-    	playerName[i]: 'Иван';
-    	playerName[i]: 'Хуан Себастьян';
-		playerName[i]: 'Мария';
-		playerName[i]: 'Кристоф';
-		playerName[i]: 'Виктор';
-		playerName[i]: 'Юлия';
-		playerName[i]: 'Люпита';
-		playerName[i]: 'Вашингтон';
-    },
-    secondName {
-    	secondName[i]: 'да Марья';
-    	secondName[i]: 'Верон';
-		secondName[i]: 'Мирабелла';
-		secondName[i]: 'Онопко';
-		secondName[i]: 'Топольницкая';
-		secondName[i]: 'Нионго';
-		secondName[i]: 'Вальц';
-		secondName[i]: 'Ирвинг';
-    },
-    coatColor {
-    	coatColor[i]:rgb(101, 137, 164);
-    	coatColor[i]:rgb(241, 43, 107);
-    	coatColor[i]:rgb(146, 100, 161);
-    	coatColor[i]:rgb(56, 159, 117);
-    	coatColor[i]:rgb(215, 210, 55);
-    	coatColor[i]:rgb(0, 0, 0);
-    },
-    eyesColour {
-    	eyesColor:'red';
-    	eyesColor:'blue';
-    	eyesColor:'black';
-    	eyesColor:'yellow';
-    	eyesColor:'green';
-    }
-];
+var NAMES = ['Иван', 'Хуан Себастьян', 'Мария', 'Кристоф', 'Виктор', 'Юлия', 'Люпита', 'Вашингтон'];
+var SECOND_NAMES = ['да Марья', 'Верон', 'Мирабелла', 'Вальц', 'Онопко', 'Топольницкая', 'Нионго', 'Ирвинг'];
+var COAT_COLOR = ['rgb(101, 137, 164)', 'rgb(241, 43, 107)', 'rgb(146, 100, 161)', 'rgb(56, 159, 117)', 'rgb(215, 210, 55)', 'rgb(0, 0, 0)'];
+var EYES_COLOR = ['black', 'red', 'blue', 'yellow', 'green'];
 var renderPlayer = function() {
     players.querySelector('.setup-similar-label').textContent = playersCharacteristics[i].name + playersCharacteristics[i].secondName;
     players.querySelector('.wizard-coat').style.fill = playersCharacteristics[i].coatColor;
